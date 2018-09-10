562262a74f4a4c272873e0ea6666b4ed0ca9c7f3
hongzy <hongzy@chigo.cn>
2018-09-10 10:06:04 +0800

    图片裁切

--- Modul/Home/ViewController/HomeViewController.m
+++ Modul/Home/ViewController/HomeViewController.m
@@ -19,6 +19,7 @@
 #import "NSCodingLearnMethod.h"
 #import <objc/runtime.h>
 #import "HZYReNameView.h"
+#import "MonitorFileChangeUtils.h"
 @interface HomeViewController ()<HomeCollectionViewDelegate,HZYRenameViewDelegate>
 /** HomeCollectionView */
 @property(nonatomic,strong)HomeCollectionView *homeCollectionView;
@@ -36,9 +37,13 @@
 @end
 
 
-@implementation HomeViewController
+@implementation HomeViewController{
+    MonitorFileChangeUtils *wather;
+    UIImageView *imageV;
+}
 
-
+// 1.文件监听
+// 2.图片截取
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     [[HZYTabbarController share] showTabbar];
@@ -48,15 +53,88 @@
     
 }
 
+/**
+ *根据给定的size的宽高比自动缩放原图片、自动判断截取位置,进行图片截取
+ * UIImage image 原始的图片
+ * CGSize size 截取图片的size
+ */
+-(UIImage *)clipImage:(UIImage *)image toRect:(CGSize)size{
+    
+    //被切图片宽比例比高比例小 或者相等，以图片宽进行放大
+    if (image.size.width*size.height <= image.size.height*size.width) {
+        
+        //以被剪裁图片的宽度为基准，得到剪切范围的大小
+        CGFloat width  = image.size.width;
+        CGFloat height = image.size.width * size.height / size.width;
+        
+        // 调用剪切方法
+        // 这里是以中心位置剪切，也可以通过改变rect的x、y值调整剪切位置
+        return [self imageFromImage:image inRect:CGRectMake(0, (image.size.height -height)/2, width, height)];
+        
+    }else{ //被切图片宽比例比高比例大，以图片高进行剪裁
+        
+        // 以被剪切图片的高度为基准，得到剪切范围的大小
+        CGFloat width  = image.size.height * size.width / size.height;
+        CGFloat height = image.size.height;
+        
+        // 调用剪切方法
+        // 这里是以中心位置剪切，也可以通过改变rect的x、y值调整剪切位置
+        return [self imageFromImage:image inRect:CGRectMake((image.size.width -width)/2, 0, width, height)];
+    }
+    return nil;
+}
+
+/**
+ *从图片中按指定的位置大小截取图片的一部分
+ * UIImage image 原始的图片
+ * CGRect rect 要截取的区域
+ */
+-(UIImage *)imageFromImage:(UIImage *)image inRect:(CGRect)rect{
+    
+    //将UIImage转换成CGImageRef
+    CGImageRef sourceImageRef = [image CGImage];
+    
+    //按照给定的矩形区域进行剪裁
+    CGImageRef newImageRef = CGImageCreateWithImageInRect(sourceImageRef, rect);
+    
+    //将CGImageRef转换成UIImage
+    UIImage *newImage = [UIImage imageWithCGImage:newImageRef];
+    
+    //返回剪裁后的图片
+    return newImage;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self p_configNavi];
-    [self.view addSubview:self.homeCollectionView];
-    [[UIApplication sharedApplication].keyWindow addSubview:self.renameView];
-    [self layoutPageViews];
-    _homeCollectionView.rowOrCol = Row;
+//    [self.view addSubview:self.homeCollectionView];
+//    [[UIApplication sharedApplication].keyWindow addSubview:self.renameView];
+//    [self layoutPageViews];
+////    _homeCollectionView.rowOrCol = Row;
+//    NSString *path = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
+//    wather =  [[MonitorFileChangeUtils alloc] init];
+//    [wather watcherForPath:path block:^(NSInteger type) {
+//        NSLog(@"%ld",type);
+//    }];
+    
+    imageV = [[UIImageView alloc] initWithFrame:CGRectMake(100, 100, 30 * 5,40 * 5 )];
+    imageV.image = [UIImage imageNamed:@"2"];
+    [self.view addSubview:imageV];
 }
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    imageV.image = [self clipImage:imageV.image toRect:CGSizeMake( 30 * 5,40 * 5)];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        imageV.image = [UIImage imageNamed:@"2"];
+    });
+}
+
+- (void)creatFileManagerObserver{
+    
+}
+
+
+
 
 
 
@@ -93,9 +171,9 @@
 //    NSString *uppercaseString = [string uppercaseString];
 //    NSLog(@"%@",uppercaseString);
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-         _homeCollectionView.rowOrCol =  !_homeCollectionView.rowOrCol;
-    });
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//         _homeCollectionView.rowOrCol =  !_homeCollectionView.rowOrCol;
+//    });
     
     
     UIButton *changeBtn = [UIButton buttonWithType:UIButtonTypeCustom];
