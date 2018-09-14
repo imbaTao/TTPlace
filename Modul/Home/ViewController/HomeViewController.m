df759bb1619a68804f36c8fb80c2b08652b2fdf8
hongzy <hongzy@chigo.cn>
2018-09-14 10:52:00 +0800

    gcd

--- Modul/Home/ViewController/HomeViewController.m
+++ Modul/Home/ViewController/HomeViewController.m
@@ -21,6 +21,7 @@
 #import "HZYReNameView.h"
 #import "MonitorFileChangeUtils.h"
 #import "MonitorFileChangeUtils+one.h"
+#import "HZYGCDLater.h"
 @interface HomeViewController ()<HomeCollectionViewDelegate,HZYRenameViewDelegate>
 /** HomeCollectionView */
 @property(nonatomic,strong)HomeCollectionView *homeCollectionView;
@@ -34,7 +35,8 @@
 /** reNameView */
 @property(nonatomic,strong)HZYReNameView *renameView;
 
-
+/** fileWatherTask */
+@property(nonatomic,copy)GCDTask fileWatherTask;
 @end
 
 
@@ -48,99 +50,144 @@
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     [[HZYTabbarController share] showTabbar];
+    
+//    [self firstMethod];
+   
+//    [self thirdMethod];
+//    [self fourthMethod];
+//    [self fivethMethod];
+//    [HZYGCDLater gcdTest];
+    
+    
+    
+  
+//    NSTimer *timer = [NSTimer timerWithTimeInterval:1 repeats:true block:^(NSTimer * _Nonnull timer) {
+//        NSLog(@"执行了");
+        if (_fileWatherTask) {
+            [HZYGCDLater gcdCancel:_fileWatherTask];
+        }
+        _fileWatherTask = [HZYGCDLater gcdDelay:2 task:^{
+            NSLog(@"oc output after 5 seconds");
+        }];
+//    }];
+//    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+//    [timer fire];
+    
+}
+
+
+
+static int count = 100000;
+- (void)firstMethod{
+    NSMutableArray *numberArr  = [NSMutableArray array];
+    for (int i = 0; i < count; i++) {
+        [numberArr addObject:[NSNumber numberWithInt:i]];
+    }
+    
+    NSTimeInterval beginTime1 = [[NSDate date] timeIntervalSince1970];
+    dispatch_queue_t  myque = dispatch_queue_create(nil, DISPATCH_QUEUE_SERIAL);
+    //    dispatch_queue_t globalQue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    // 串行队列同步执行
+    dispatch_async(myque, ^{
+        [numberArr enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//                NSLog(@"%ld - arrayEnumerator-1",[obj integerValue]);
+            if (idx == numberArr.count - 1) {
+                NSTimeInterval endTime1 = [[NSDate date] timeIntervalSince1970];
+                NSLog(@"%f-time1",endTime1  - beginTime1);
+                 [self secondMethod];
+            }
+        }];
+    });
+}
+
+
+
+- (void)secondMethod{
+    NSMutableArray *numberArr  = [NSMutableArray array];
+    for (int i = 0; i < count; i++) {
+        [numberArr addObject:[NSNumber numberWithInt:i]];
+    }
+    NSTimeInterval beginTime1 = [[NSDate date] timeIntervalSince1970];
+    [numberArr enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//            NSLog(@"%ld - arrayEnumerator-2",[obj integerValue]);
+    }];
+    NSTimeInterval endTime1 = [[NSDate date] timeIntervalSince1970];
+    NSLog(@"%f-time2",endTime1  - beginTime1);
+    [self thirdMethod];
+}
+
+
+- (void)thirdMethod{
+    NSMutableArray *numberArr  = [NSMutableArray array];
+    for (int i = 0; i < count; i++) {
+        [numberArr addObject:[NSNumber numberWithInt:i]];
+    }
+    NSTimeInterval beginTime1 = [[NSDate date] timeIntervalSince1970];
+    for (int i = 0; i < numberArr.count; i++) {
+//        NSLog(@"%d - arrayEnumerator-3",i);
+    }
+    NSTimeInterval endTime1 = [[NSDate date] timeIntervalSince1970];
+    NSLog(@"%f-time3",endTime1  - beginTime1);
+    [self fourthMethod];
+}
+
+- (void)fourthMethod{
+    NSMutableArray *numberArr  = [NSMutableArray array];
+    for (int i = 0; i < count; i++) {
+        [numberArr addObject:[NSNumber numberWithInt:i]];
+    }
+    NSTimeInterval beginTime1 = [[NSDate date] timeIntervalSince1970];
+    for (NSNumber *number in numberArr) {
+//        NSLog(@"%d - arrayEnumerator-4",[number intValue]);
+    }
+    NSTimeInterval endTime1 = [[NSDate date] timeIntervalSince1970];
+    NSLog(@"%f-time4-遍历",endTime1  - beginTime1);
+    [self fivethMethod];
+}
+
+
+- (void)fivethMethod{
+    NSMutableArray *numberArr  = [NSMutableArray array];
+    for (int i = 0; i < count; i++) {
+        [numberArr addObject:[NSNumber numberWithInt:i]];
+    }
+    NSTimeInterval beginTime1 = [[NSDate date] timeIntervalSince1970];
+    dispatch_queue_t  myque = dispatch_queue_create(nil, DISPATCH_QUEUE_SERIAL);
+    dispatch_async(myque, ^{
+        for (NSNumber *number in numberArr) {
+//            NSLog(@"%d - arrayEnumerator-5",[number intValue]);
+        }
+        NSTimeInterval endTime1 = [[NSDate date] timeIntervalSince1970];
+        NSLog(@"%f-time5-队列遍历",endTime1  - beginTime1);
+        [self sixthMethod];
+    });
+}
+
+- (void)sixthMethod{
+    NSTimeInterval beginTime1 = [[NSDate date] timeIntervalSince1970];
+    dispatch_queue_t queue = dispatch_get_global_queue(0, 0);
+    dispatch_apply(count, queue, ^(size_t insex) {
+        if (insex == count - 1) {
+             NSTimeInterval endTime1 = [[NSDate date] timeIntervalSince1970];
+             NSLog(@"%f-time6-disPath遍历",endTime1  - beginTime1);
+        }
+    });
+}
+
+
+- (void)forinWithArray:(NSMutableArray *)arr class:(id)class{
+//    for ( in <#collection#>) {
+//        <#statements#>
+//    }
 }
 
 - (void)viewWillDisappear:(BOOL)animated{
-    
-}
-
-/**
- *根据给定的size的宽高比自动缩放原图片、自动判断截取位置,进行图片截取
- * UIImage image 原始的图片
- * CGSize size 截取图片的size
- */
--(UIImage *)clipImage:(UIImage *)image toRect:(CGSize)size{
-    
-    //被切图片宽比例比高比例小 或者相等，以图片宽进行放大
-    if (image.size.width*size.height <= image.size.height*size.width) {
-        
-        //以被剪裁图片的宽度为基准，得到剪切范围的大小
-        CGFloat width  = image.size.width;
-        CGFloat height = image.size.width * size.height / size.width;
-        
-        // 调用剪切方法
-        // 这里是以中心位置剪切，也可以通过改变rect的x、y值调整剪切位置
-        return [self imageFromImage:image inRect:CGRectMake(0, (image.size.height -height)/2, width, height)];
-        
-    }else{ //被切图片宽比例比高比例大，以图片高进行剪裁
-        
-        // 以被剪切图片的高度为基准，得到剪切范围的大小
-        CGFloat width  = image.size.height * size.width / size.height;
-        CGFloat height = image.size.height;
-        
-        // 调用剪切方法
-        // 这里是以中心位置剪切，也可以通过改变rect的x、y值调整剪切位置
-        return [self imageFromImage:image inRect:CGRectMake((image.size.width -width)/2, 0, width, height)];
-    }
-    return nil;
-}
-
-/**
- *从图片中按指定的位置大小截取图片的一部分
- * UIImage image 原始的图片
- * CGRect rect 要截取的区域
- */
--(UIImage *)imageFromImage:(UIImage *)image inRect:(CGRect)rect{
-    
-    //将UIImage转换成CGImageRef
-    CGImageRef sourceImageRef = [image CGImage];
-    
-    //按照给定的矩形区域进行剪裁
-    CGImageRef newImageRef = CGImageCreateWithImageInRect(sourceImageRef, rect);
-    
-    //将CGImageRef转换成UIImage
-    UIImage *newImage = [UIImage imageWithCGImage:newImageRef];
-    
-    //返回剪裁后的图片
-    return newImage;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self p_configNavi];
-//    [self.view addSubview:self.homeCollectionView];
-//    [[UIApplication sharedApplication].keyWindow addSubview:self.renameView];
-//    [self layoutPageViews];
-////    _homeCollectionView.rowOrCol = Row;
-    NSString *path = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents"];
-    watcher =  [[MonitorFileChangeUtils alloc] init];
-    watcher.rankNumber = @"5";
-     NSLog(@"%@",watcher.rankNumber);
-    [watcher watcherForPath:path block:^(NSInteger type) {
-        NSLog(@"%ld",type);
-    }];
-    
-//    imageV = [[UIImageView alloc] initWithFrame:CGRectMake(100, 100, 30 * 5,40 * 5 )];
-//    imageV.image = [UIImage imageNamed:@"2"];
-//    [self.view addSubview:imageV];
 }
-
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    imageV.image = [self clipImage:imageV.image toRect:CGSizeMake( 30 * 5,40 * 5)];
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        imageV.image = [UIImage imageNamed:@"2"];
-    });
-}
-
-- (void)creatFileManagerObserver{
-    
-}
-
-
-
-
-
-
 
 - (void)layoutPageViews{
     self.homeCollectionView.dataArray = [NSMutableArray arrayWithArray:@[@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9"]];
@@ -151,33 +198,6 @@
     [self.renameView mas_makeConstraints:^(MASConstraintMaker *make) {
          make.edges.mas_equalTo(UIEdgeInsetsMake(0, 0, 0, 0));
     }];
-    
-
-//    NSString *dateStr = @"2018-7-25";
-//    NSDateFormatter *fomater = [[NSDateFormatter alloc] init];
-//    fomater.dateFormat = @"yyyy-MM-dd";
-//    fomater.timeZone =  [NSTimeZone defaultTimeZone];
-//    [fomater dateFromString:dateStr];
-    
-    
-//    self.tempView = [[UIButton alloc] initWithFrame:CGRectMake(200, 30, 50, 50)];
-//    self.tempView.backgroundColor = [UIColor redColor];
-//    [self.tempView addTarget:self action:@selector(bublleAction) forControlEvents:UIControlEventTouchUpInside];
-//    [self.view addSubview:self.tempView];
-//    Method originalMethod = class_getClassMethod([NSString class], @selector(lowercaseString));
-//    Method swappedMethod = class_getClassMethod([NSString class], @selector(uppercaseString));
-//    method_exchangeImplementations(originalMethod, swappedMethod);
-//
-//    NSString *string = @"ThIs iS tHe StRiNg";
-//    NSString *lowercaseString = [string lowercaseString];
-//    NSLog(@"%@",lowercaseString);
-//    NSString *uppercaseString = [string uppercaseString];
-//    NSLog(@"%@",uppercaseString);
-    
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//         _homeCollectionView.rowOrCol =  !_homeCollectionView.rowOrCol;
-//    });
-    
     
     UIButton *changeBtn = [UIButton buttonWithType:UIButtonTypeCustom];
     changeBtn.frame = CGRectMake(SCREEN_W - 120, SCREEN_H - 200, 70, 70);
