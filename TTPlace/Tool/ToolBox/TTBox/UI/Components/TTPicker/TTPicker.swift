aec6b3a01cafd4955e32dfb371f62868379a9d23
hong <imbaTao@outlook.com>
2020-12-05 17:51:13 +0800

    picker 封装

--- TTPlace/Tool/ToolBox/TTBox/UI/Components/TTPicker/TTPicker.swift
+++ TTPlace/Tool/ToolBox/TTBox/UI/Components/TTPicker/TTPicker.swift
@@ -9,10 +9,36 @@ import UIKit
 
 
 class TTPickerToolBar: UIView {
-//    lazy var leftButton: UIButton = {
-//        var <#name#> = <#what#>
-//        return <#name#>
-//    }()
+    lazy var leftButton: UIButton = {
+        var leftButton = UIButton.title(title: "取消", titleColor: .gray, font: .regular(18))
+        addSubview(leftButton)
+        leftButton.snp.makeConstraints { (make) in
+            make.centerY.equalToSuperview()
+            make.left.equalTo(hor(12))
+        }
+        return leftButton
+    }()
+    
+    lazy var rightButton: UIButton = {
+        var rightButton = UIButton.title(title: "确定", titleColor: .red, font: .regular(18))
+        addSubview(rightButton)
+        rightButton.snp.makeConstraints { (make) in
+            make.centerY.equalToSuperview()
+            make.right.equalTo(hor(-10))
+        }
+        return rightButton
+    }()
+    
+    // 标题
+    lazy var title: UILabel = {
+        var title = UILabel.regular(size: 16, textColor: .black, text: "pickerView标题", alignment: .center)
+        addSubview(title)
+        title.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+        
+        }
+        return title
+    }()
 }
 
 class TTPicker: UIPickerView {
@@ -23,10 +49,28 @@ class TTPicker: UIPickerView {
         return toolBar
     }()
     
+    private var segmentLineWidth = hor(260)
     
+//    override init(frame: CGRect) {
+//        super.init(frame: .zero)
+//
+//    }
+
+    convenience init(segmentLineWidth: CGFloat) {
+        self.init(frame: .zero)
+        self.segmentLineWidth = segmentLineWidth
+    }
+
+    
+    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
+    
+
     override func layoutSubviews() {
         super.layoutSubviews()
-        
         
         for item in self.subviews {
             
@@ -39,7 +83,7 @@ class TTPicker: UIPickerView {
                 item.addSubview(topLine)
                 topLine.snp.makeConstraints { (make) in
                     make.top.equalTo(0)
-                    make.width.equalTo(hor(260))
+                    make.width.equalTo(segmentLineWidth)
                     make.centerX.equalToSuperview()
                     make.height.equalTo(1)
                 }
@@ -50,7 +94,7 @@ class TTPicker: UIPickerView {
                 item.addSubview(bottomLine)
                 bottomLine.snp.makeConstraints { (make) in
                     make.bottom.equalTo(0)
-                    make.width.equalTo(hor(260))
+                    make.width.equalTo(segmentLineWidth)
                     make.height.equalTo(1)
                     make.centerX.equalToSuperview()
                 }
