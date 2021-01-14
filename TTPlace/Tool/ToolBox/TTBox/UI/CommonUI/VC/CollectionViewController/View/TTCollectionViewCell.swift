c581e65507c1b0524cb5d0fe6951c92f31f89062
hong <imbaTao@outlook.com>
2021-01-14 17:54:53 +0800

    准备提测前

--- TTPlace/Tool/ToolBox/TTBox/UI/CommonUI/VC/CollectionViewController/View/TTCollectionViewCell.swift
+++ TTPlace/Tool/ToolBox/TTBox/UI/CommonUI/VC/CollectionViewController/View/TTCollectionViewCell.swift
@@ -108,63 +108,66 @@ class TTCollectionViewCell: UICollectionViewCell {
     func bind(to viewModel: TTCollectionViewCellViewModel) {
         
         // 赋值，没有值就隐藏
-        viewModel.mainContent.asDriver().drive(mainLabel.rx.text).disposed(by: rx.disposeBag)
-        viewModel.mainContent.asDriver().replaceNilWith("").map { $0.isEmpty }.drive(mainLabel.rx.isHidden).disposed(by: rx.disposeBag)
+        viewModel.mainContent.asDriver().drive(mainLabel.rx.text).disposed(by: cellDisposeBag)
+        viewModel.mainContent.asDriver().replaceNilWith("").map { $0.isEmpty }.drive(mainLabel.rx.isHidden).disposed(by: cellDisposeBag)
 
         // 是子内容
-        viewModel.subContent.asDriver().drive(subLabel.rx.text).disposed(by: rx.disposeBag)
-        viewModel.subContent.asDriver().replaceNilWith("").map { $0.isEmpty }.drive(subLabel.rx.isHidden).disposed(by: rx.disposeBag)
+        viewModel.subContent.asDriver().drive(subLabel.rx.text).disposed(by: cellDisposeBag)
+        viewModel.subContent.asDriver().replaceNilWith("").map { $0.isEmpty }.drive(subLabel.rx.isHidden).disposed(by: cellDisposeBag)
 
         //  第二条子内容
-        viewModel.secondSubContent.asDriver().drive(secondSubLabel.rx.text).disposed(by: rx.disposeBag)
-        viewModel.secondSubContent.asDriver().replaceNilWith("").map { $0.isEmpty }.drive(secondSubLabel.rx.isHidden).disposed(by: rx.disposeBag)
+        viewModel.secondSubContent.asDriver().drive(secondSubLabel.rx.text).disposed(by: cellDisposeBag)
+        viewModel.secondSubContent.asDriver().replaceNilWith("").map { $0.isEmpty }.drive(secondSubLabel.rx.isHidden).disposed(by: cellDisposeBag)
 
         
 
-//        viewModel.hidesDisclosure.asDriver().drive(rightImageView.rx.isHidden).disposed(by: rx.disposeBag)
+//        viewModel.hidesDisclosure.asDriver().drive(rightImageView.rx.isHidden).disposed(by: cellDisposeBag)
 
         // 头像
         viewModel.avatarImage.asDriver().filterNil()
-            .drive(avatar.rx.image).disposed(by: rx.disposeBag)
+            .drive(avatar.rx.image).disposed(by: cellDisposeBag)
         viewModel.avatarImageUrl.map { $0?.url }.asDriver(onErrorJustReturn: nil).filterNil()
-            .drive(avatar.rx.imageURL).disposed(by: rx.disposeBag)
+            .drive(avatar.rx.imageURL).disposed(by: cellDisposeBag)
         viewModel.avatarImageUrl.asDriver().filterNil()
             .drive(onNext: { [weak self] (url) in
                 self?.avatar.hero.id = url
-            }).disposed(by: rx.disposeBag)
+            }).disposed(by: cellDisposeBag)
+     
         
         
         // 左侧图片
         viewModel.leftImage.asDriver().filterNil()
-            .drive(leftImageView.rx.image).disposed(by: rx.disposeBag)
+            .drive(leftImageView.rx.image).disposed(by: cellDisposeBag)
         viewModel.leftImageUrl.map { $0?.url }.asDriver(onErrorJustReturn: nil).filterNil()
-            .drive(leftImageView.rx.imageURL).disposed(by: rx.disposeBag)
+            .drive(leftImageView.rx.imageURL).disposed(by: cellDisposeBag)
         viewModel.leftImageUrl.asDriver().filterNil()
             .drive(onNext: { [weak self] (url) in
                 self?.leftImageView.hero.id = url
-            }).disposed(by: rx.disposeBag)
+            }).disposed(by: cellDisposeBag)
+        viewModel.hideLeftImage.bind(to: leftImageView.rx.isHidden).disposed(by: cellDisposeBag)
         
         
         // 中心图片
         viewModel.centerImage.asDriver().filterNil()
-            .drive(centerImageView.rx.image).disposed(by: rx.disposeBag)
+            .drive(centerImageView.rx.image).disposed(by: cellDisposeBag)
         viewModel.centerImageUrl.map { $0?.url }.asDriver(onErrorJustReturn: nil).filterNil()
-            .drive(centerImageView.rx.imageURL).disposed(by: rx.disposeBag)
+            .drive(centerImageView.rx.imageURL).disposed(by: cellDisposeBag)
         viewModel.centerImageUrl.asDriver().filterNil()
             .drive(onNext: { [weak self] (url) in
                 self?.centerImageView.hero.id = url
-            }).disposed(by: rx.disposeBag)
-        
+            }).disposed(by: cellDisposeBag)
+        viewModel.hideCenterImage.bind(to: centerImageView.rx.isHidden).disposed(by: cellDisposeBag)
         
         // 右侧图片
         viewModel.rightImage.asDriver().filterNil()
-            .drive(rightImageView.rx.image).disposed(by: rx.disposeBag)
+            .drive(rightImageView.rx.image).disposed(by: cellDisposeBag)
         viewModel.rightImageUrl.map { $0?.url }.asDriver(onErrorJustReturn: nil).filterNil()
-            .drive(rightImageView.rx.imageURL).disposed(by: rx.disposeBag)
+            .drive(rightImageView.rx.imageURL).disposed(by: cellDisposeBag)
         viewModel.rightImageUrl.asDriver().filterNil()
             .drive(onNext: { [weak self] (url) in
                 self?.rightImageView.hero.id = url
-            }).disposed(by: rx.disposeBag)
+            }).disposed(by: cellDisposeBag)
+        viewModel.hideRightImage.bind(to: rightImageView.rx.isHidden).disposed(by: cellDisposeBag)
         
     }
     
